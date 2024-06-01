574c61c1ac8763bc3665387eb5a7e9ad669a7bbd
amlndz <alejandromelendez2014@gmail.com>
2024-06-01 12:36:30 +0200

    Arreglar Footer responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import style from './Footer.module.css'
 export const Footer = () => {
   return (
     <footer className={style['footer']}>
-      <p>
+      <p className={style['footer-msg']}>
         ©2024 Pokémon. ©1995 - 2024 Nintendo/Creatures Inc./GAME FREAK inc. TM,
         ®Nintendo.
       </p>
