0283a7a98e2bf0fbeffc836673163cede9f7e5cc
Zhaklina Dobromirova <zhaklina@Air-de-Zhaklina.gestionwifi>
2024-06-01 12:27:21 +0200

    Footer test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -7,4 +7,9 @@ import { expect } from 'vitest'
 test('Si cargamos la pagina se mostraran los componentes base de la pagina', () => {
   render(<App />)
   expect(screen.getByText('Pokédex')).toBeInTheDocument()
+  expect(
+    screen.getByText(
+      '©2024 Pokémon. ©1995 - 2024 Nintendo/Creatures Inc./GAME FREAK inc. TM, ®Nintendo.',
+    ),
+  ).toBeInTheDocument()
 })
