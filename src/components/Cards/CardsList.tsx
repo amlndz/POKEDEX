ba347b9349ef62d7a07ed90f8cbe2afb736b608f
amlndz <alejandromelendez2014@gmail.com>
2024-06-04 18:47:32 +0200

    defautls cards

--- src/components/Cards/CardsList.tsx
+++ src/components/Cards/CardsList.tsx
@@ -1,9 +1,18 @@
 import { DefaultCard } from './DefaultCard/DefaultCard'
+import style from './CardList.module.css'
 
 export const CardList = () => {
   return (
-    <>
+    <div className={style['containerCardsList']}>
       <DefaultCard />
-    </>
+      <DefaultCard />
+      <DefaultCard />
+      <DefaultCard />
+      <DefaultCard />
+      <DefaultCard />
+      <DefaultCard />
+      <DefaultCard />
+      <DefaultCard />
+    </div>
   )
 }
