6faa53bb7ade0a6406924545b27b0ff76c7952f5
amlndz <alejandromelendez2014@gmail.com>
2024-06-04 19:54:07 +0200

    Arreglar problema footer

--- src/components/Cards/CardsList.tsx
+++ src/components/Cards/CardsList.tsx
@@ -1,18 +1,11 @@
-import { DefaultCard } from './DefaultCard/DefaultCard'
+// import { DefaultCard } from './DefaultCard/DefaultCard';
 import style from './CardList.module.css'
+import { ErrorMSG } from './Error/ErrorMSG'
 
 export const CardList = () => {
   return (
     <div className={style['containerCardsList']}>
-      <DefaultCard />
-      <DefaultCard />
-      <DefaultCard />
-      <DefaultCard />
-      <DefaultCard />
-      <DefaultCard />
-      <DefaultCard />
-      <DefaultCard />
-      <DefaultCard />
+      <ErrorMSG />
     </div>
   )
 }
---
+++ src/components/Cards/Error/ErrorMSG.tsx
@@ -0,0 +1,15 @@
+import style from './ErrorMSG.module.css'
+
+export const ErrorMSG = () => {
+  return (
+    <div className={style['containerError']}>
+      <img
+        className={style['imagenError']}
+        src="./assets/alert.svg"
+        alt="Imagen de error"
+      />
+      <p className={style['textoError']}>An error ocurred getting Pokémons.</p>
+      <p className={style['textoError']}>Please, try it later</p>
+    </div>
+  )
+}
