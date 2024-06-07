2d54635fa373d5f33d25eef2c3c742bbb1b2a632
amlndz <alejandromelendez2014@gmail.com>
2024-06-07 19:05:22 +0200

    Manejo error/lista nombres pokemon

--- src/components/Cards/CardsList.tsx
+++ src/components/Cards/CardsList.tsx
@@ -1,13 +1,48 @@
 // import { DefaultCard } from './DefaultCard/DefaultCard'
-// import { ErrorMSG } from './Error/ErrorMSG'
-import { NotFound } from './NotFound/NotFound'
+import { ErrorMSG } from './Error/ErrorMSG'
+// import { NotFound } from './NotFound/NotFound'
 import style from './CardList.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+// import { ErrorMSG } from './Error/ErrorMSG'
 
+type PokedexDTO = {
+  name: string
+  url: string
+}
+
+type RespuestaApi = {
+  count: number
+  next: string | null
+  previous: string | null
+  results: PokedexDTO[]
+}
 export const CardList = () => {
+  const [pokedex, setPokedex] = useState<PokedexDTO[]>([])
+  const [apiError, setApiError] = useState(false)
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await axios.get<RespuestaApi>(
+          'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0',
+        )
+        setPokedex(response.data.results)
+        setApiError(false)
+      } catch {
+        setApiError(true)
+        setPokedex([])
+        console.log('[!] - Error en la respuesta de la API')
+      }
+    }
+    fetchPokemons()
+  }, [])
   return (
     <div className={style['containerCardsList']}>
-      {/* <ErrorMSG /> */}
-      <NotFound />
+      {apiError && <ErrorMSG />}
+
+      {pokedex.map(pokemon => (
+        <p>{pokemon.name}</p>
+      ))}
       {/* <DefaultCard /> */}
     </div>
   )
