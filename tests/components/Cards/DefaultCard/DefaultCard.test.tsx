61dc38154e715c315cf37984d66ac658345a3296
amlndz <alejandromelendez2014@gmail.com>
2024-06-11 23:58:20 +0200

    Test ErrorMSG

--- tests/components/Cards/DefaultCard/DefaultCard.test.tsx
+++ tests/components/Cards/DefaultCard/DefaultCard.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
-import App from '../../../src/App'
+import App from '../../../../src/App'
 import '@testing-library/jest-dom'
 import { expect } from 'vitest'
 import axios from 'axios'
