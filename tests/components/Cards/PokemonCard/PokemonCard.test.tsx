8a4b9693528101759c3770070079df7ff0a2d0e1
amlndz <alejandromelendez2014@gmail.com>
2024-06-12 00:34:50 +0200

    Busqueda 1 pokemon

--- tests/components/Cards/PokemonCard/PokemonCard.test.tsx
+++ tests/components/Cards/PokemonCard/PokemonCard.test.tsx
@@ -54,3 +54,48 @@ test('Se cargan los 151 pokemons', async () => {
   const pokemonCardElements = await screen.findAllByTestId('pokemon-card')
   expect(pokemonCardElements.length).toBe(151)
 })
+
+test('Si un usuario busca un pokemon existente solo se muestra este', async () => {
+  const mock = new MockAdapter(axios)
+
+  mock
+    .onGet('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    .reply(200, {
+      count: 1,
+      next: null,
+      previous: null,
+      results: [
+        { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+      ],
+    })
+
+  mock.onGet('https://pokeapi.co/api/v2/pokemon/1/').reply(200, {
+    name: 'bulbasaur',
+    id: 1,
+    sprites: {
+      other: {
+        'official-artwork': {
+          front_default: 'bulbasaur-image-url',
+        },
+      },
+    },
+    types: [{ type: { name: 'grass' } }, { type: { name: 'poison' } }],
+    weight: 69,
+    height: 7,
+    stats: [
+      { base_stat: 45, stat: { name: 'hp' } },
+      { base_stat: 49, stat: { name: 'attack' } },
+      { base_stat: 49, stat: { name: 'defense' } },
+      { base_stat: 65, stat: { name: 'special-attack' } },
+      { base_stat: 65, stat: { name: 'special-defense' } },
+      { base_stat: 45, stat: { name: 'speed' } },
+    ],
+  })
+
+  render(<App />)
+  const input = screen.getByRole('textbox')
+  fireEvent.change(input, { target: { value: 'bulbasaur' } })
+  await screen.findByText('bulbasaur')
+  const pokemonCardElements = await screen.findAllByTestId('pokemon-card')
+  expect(pokemonCardElements.length).toBe(1)
+})
