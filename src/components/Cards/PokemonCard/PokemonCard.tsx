f3db2e6921caeeb5290e93ae48833f009e79a0a3
Zhaklina Dobromirova <zhaklina@MacBook-Air-de-Zhaklina.local>
2024-06-12 16:45:01 +0200

    Estilado PokemonCard

--- src/components/Cards/PokemonCard/PokemonCard.tsx
+++ src/components/Cards/PokemonCard/PokemonCard.tsx
@@ -8,11 +8,15 @@ type PokemonCardProps = {
 const DEFAULT_IMAGE_URL = './assets/pokeballDefault.png'
 
 export const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
+  const formatearStat = (stat: number) =>
+    stat < 100 ? `0${stat}` : stat.toString()
   return (
     <div className={style['pokemon-card']} data-testid="pokemon-card">
       <div className={style['pokemon-header']}>
-        <h2>{pokemon.name}</h2>
-        <h2>#{pokemon.id.toString().padStart(3, '0')}</h2>
+        <h2 className={style['pokemon-nombre']}>{pokemon.name}</h2>
+        <h2 className={style['pokemon-id']}>
+          #{pokemon.id.toString().padStart(3, '0')}
+        </h2>
       </div>
       <div className={style['pokemon-content']}>
         <img
@@ -37,37 +41,37 @@ export const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
         <ul className={style['pokemon-stats']}>
           <li>
             <div className={style['pokemon-stat']}>
-              <p>HP</p> <span>{pokemon.stats.hp}</span>
+              <p>HP</p> <span>{formatearStat(pokemon.stats.hp)}</span>
             </div>
             <progress max={255} value={pokemon.stats.hp}></progress>
           </li>
           <li>
             <div className={style['pokemon-stat']}>
-              <p>ATK</p> <span>{pokemon.stats.attack}</span>
+              <p>ATK</p> <span>{formatearStat(pokemon.stats.attack)}</span>
             </div>
             <progress max={255} value={pokemon.stats.attack}></progress>
           </li>
           <li>
             <div className={style['pokemon-stat']}>
-              <p>DEF</p> <span>{pokemon.stats.defense}</span>
+              <p>DEF</p> <span>{formatearStat(pokemon.stats.defense)}</span>
             </div>
             <progress max={255} value={pokemon.stats.defense}></progress>
           </li>
           <li>
             <div className={style['pokemon-stat']}>
-              <p>SAT</p> <span>{pokemon.stats.spattack}</span>
+              <p>SAT</p> <span>{formatearStat(pokemon.stats.spattack)}</span>
             </div>
             <progress max={255} value={pokemon.stats.spattack}></progress>
           </li>
           <li>
             <div className={style['pokemon-stat']}>
-              <p>SDF</p> <span>{pokemon.stats.spdefense}</span>
+              <p>SDF</p> <span>{formatearStat(pokemon.stats.spdefense)}</span>
             </div>
             <progress max={255} value={pokemon.stats.spdefense}></progress>
           </li>
           <li>
             <div className={style['pokemon-stat']}>
-              <p>SPD</p> <span>{pokemon.stats.speed}</span>
+              <p>SPD</p> <span>{formatearStat(pokemon.stats.speed)}</span>
             </div>
             <progress max={255} value={pokemon.stats.speed}></progress>
           </li>
