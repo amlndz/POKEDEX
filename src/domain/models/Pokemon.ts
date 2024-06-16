9a155d2171069217895e81a8fa5cf3f936116001
amlndz <alejandromelendez2014@gmail.com>
2024-06-16 11:20:46 +0200

    Añadir PokemonType

--- src/domain/models/Pokemon.ts
+++ src/domain/models/Pokemon.ts
@@ -1,8 +1,28 @@
+export type PokemonType =
+  | 'grass'
+  | 'bug'
+  | 'electric'
+  | 'ground'
+  | 'fire'
+  | 'fighting'
+  | 'poison'
+  | 'psychic'
+  | 'fairy'
+  | 'dragon'
+  | 'flying'
+  | 'ghost'
+  | 'water'
+  | 'ice'
+  | 'normal'
+  | 'rock'
+  | 'steel'
+  | 'dark'
+
 export type Pokemon = {
   name: string
   id: number
   image: string
-  type: string[]
+  type: PokemonType[]
   weight: number
   height: number
   stats: {
