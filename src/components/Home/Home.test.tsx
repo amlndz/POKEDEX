1d45a64c880992426c16aaa2dd44fef404e40401
amlndz <alejandromelendez2014@gmail.com>
2024-06-21 10:33:36 +0200

    Mejorar Tests

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -12,59 +12,8 @@ beforeEach(() => {
   pokemonService.init(pokemonAPIRepository)
 })
 
-describe('Tests renderizado DefaultCards', () => {
-  const mock = new MockAdapter(axios)
-
-  mock
-    .onGet('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
-    .reply(200, {
-      count: 1,
-      next: null,
-      previous: null,
-      results: [
-        { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
-      ],
-    })
-
-  test('Mientras se cargan las peticiones se muestra DefaultCard', async () => {
-    render(<Home />)
-
-    const defaultCardElements = await screen.findAllByTestId('default-card')
-    expect(defaultCardElements.length).toBeGreaterThan(0)
-  })
-})
-
-describe('Test renderizado ErrorMsg', () => {
-  test('Si se da un error la API se muestra errorMSG', async () => {
-    const mock = new MockAdapter(axios)
-    mock
-      .onGet('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
-      .reply(500)
-
-    render(<Home />)
-
-    await waitFor(() => {
-      expect(
-        screen.getByText('An error ocurred getting Pokémons.'),
-      ).toBeInTheDocument()
-    })
-  })
-
-  test('Si se da pide a la API una url inexistente se muestra errorMSG', async () => {
-    const mock = new MockAdapter(axios)
-    mock.onGet('https://pokeapi.co/api/v2/nonexist').reply(400)
-    render(<Home />)
-
-    await waitFor(() => {
-      expect(
-        screen.getByText('An error ocurred getting Pokémons.'),
-      ).toBeInTheDocument()
-    })
-  })
-})
-
-describe('Test renderizado NotFound', () => {
-  test('Si un usuario introduce un pokemon que no existe se muestra NotFound', async () => {
+describe('Test Home', () => {
+  describe('Tests renderizado DefaultCards', () => {
     const mock = new MockAdapter(axios)
 
     mock
@@ -78,131 +27,186 @@ describe('Test renderizado NotFound', () => {
         ],
       })
 
-    mock.onGet('https://pokeapi.co/api/v2/pokemon/1/').reply(200, {
-      name: 'bulbasaur',
-      id: 1,
-      sprites: {
-        other: {
-          'official-artwork': {
-            front_default: 'bulbasaur-image-url',
+    test('Mientras se cargan las peticiones se muestra DefaultCard', async () => {
+      render(<Home />)
+
+      const defaultCardElements = await screen.findAllByTestId('default-card')
+      expect(defaultCardElements.length).toBeGreaterThan(0)
+    })
+  })
+
+  describe('Test renderizado ErrorMsg', () => {
+    test('Si se da un error la API se muestra errorMSG', async () => {
+      const mock = new MockAdapter(axios)
+      mock
+        .onGet('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+        .reply(500)
+
+      render(<Home />)
+
+      await waitFor(() => {
+        expect(
+          screen.getByText('An error ocurred getting Pokémons.'),
+        ).toBeInTheDocument()
+      })
+    })
+
+    test('Si se da pide a la API una url inexistente se muestra errorMSG', async () => {
+      const mock = new MockAdapter(axios)
+      mock.onGet('https://pokeapi.co/api/v2/nonexist').reply(400)
+      render(<Home />)
+
+      await waitFor(() => {
+        expect(
+          screen.getByText('An error ocurred getting Pokémons.'),
+        ).toBeInTheDocument()
+      })
+    })
+  })
+
+  describe('Test renderizado NotFound', () => {
+    test('Si un usuario introduce un pokemon que no existe se muestra NotFound', async () => {
+      const mock = new MockAdapter(axios)
+
+      mock
+        .onGet('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+        .reply(200, {
+          count: 1,
+          next: null,
+          previous: null,
+          results: [
+            { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+          ],
+        })
+
+      mock.onGet('https://pokeapi.co/api/v2/pokemon/1/').reply(200, {
+        name: 'bulbasaur',
+        id: 1,
+        sprites: {
+          other: {
+            'official-artwork': {
+              front_default: 'bulbasaur-image-url',
+            },
           },
         },
-      },
-      types: [{ type: { name: 'grass' } }, { type: { name: 'poison' } }],
-      weight: 69,
-      height: 7,
-      stats: [
-        { base_stat: 45, stat: { name: 'hp' } },
-        { base_stat: 49, stat: { name: 'attack' } },
-        { base_stat: 49, stat: { name: 'defense' } },
-        { base_stat: 65, stat: { name: 'special-attack' } },
-        { base_stat: 65, stat: { name: 'special-defense' } },
-        { base_stat: 45, stat: { name: 'speed' } },
-      ],
-    })
-    render(<Home />)
+        types: [{ type: { name: 'grass' } }, { type: { name: 'poison' } }],
+        weight: 69,
+        height: 7,
+        stats: [
+          { base_stat: 45, stat: { name: 'hp' } },
+          { base_stat: 49, stat: { name: 'attack' } },
+          { base_stat: 49, stat: { name: 'defense' } },
+          { base_stat: 65, stat: { name: 'special-attack' } },
+          { base_stat: 65, stat: { name: 'special-defense' } },
+          { base_stat: 45, stat: { name: 'speed' } },
+        ],
+      })
+      render(<Home />)
 
-    const input = screen.getByRole('textbox')
-    fireEvent.change(input, { target: { value: 'nonexistent' } })
+      const input = screen.getByRole('textbox')
+      fireEvent.change(input, { target: { value: 'nonexistent' } })
 
-    await screen.findByText('There is not results for "nonexistent".')
-    const pokemonCardElements = screen.queryAllByTestId('pokemon-card')
-    expect(pokemonCardElements.length).toBe(0)
-  })
-})
-
-describe('Test renderizado Pokemon Cards', () => {
-  test('Renderizado de las cards de pokemons', async () => {
-    const pokemon = new PokemonBuilder().build()
-    const serviceMock = vi
-      .spyOn(pokemonService, 'obtainPokemons')
-      .mockResolvedValue([pokemon])
-
-    render(<Home />)
-    expect(serviceMock).toHaveBeenCalledTimes(1)
-
-    await waitFor(() => {
-      expect(screen.getByText('Bulbasaur')).toBeInTheDocument()
-      expect(screen.getByText(`#001`)).toBeInTheDocument()
-      expect(screen.getByText('Grass')).toBeInTheDocument()
-      expect(screen.getByText('Poison')).toBeInTheDocument()
-      expect(screen.getByAltText('bulbasaur')).toHaveAttribute(
-        'src',
-        'bulbasaur-image-url',
-      )
-      expect(screen.getByText('6.9 kg')).toBeInTheDocument()
-      expect(screen.getByText('0.7 m')).toBeInTheDocument()
-
-      expect(screen.getByText('HP').nextElementSibling).toHaveTextContent('045')
-      expect(screen.getByText('ATK').nextElementSibling).toHaveTextContent(
-        '049',
-      )
-      expect(screen.getByText('DEF').nextElementSibling).toHaveTextContent(
-        '049',
-      )
-      expect(screen.getByText('SAT').nextElementSibling).toHaveTextContent(
-        '065',
-      )
-      expect(screen.getByText('SDF').nextElementSibling).toHaveTextContent(
-        '065',
-      )
-      expect(screen.getByText('SPD').nextElementSibling).toHaveTextContent(
-        '045',
-      )
+      await screen.findByText('There is not results for "nonexistent".')
+      const pokemonCardElements = screen.queryAllByTestId('pokemon-card')
+      expect(pokemonCardElements.length).toBe(0)
     })
   })
 
-  test('Si un usuario busca un pokemon existente solo se muestra este', async () => {
-    const pokemon = new PokemonBuilder().build()
+  describe('Test renderizado Pokemon Cards', () => {
+    test('Renderizado de las cards de pokemons', async () => {
+      const pokemon = new PokemonBuilder().build()
+      const serviceMock = vi
+        .spyOn(pokemonService, 'obtainPokemons')
+        .mockResolvedValue([pokemon])
 
-    const serviceMock = vi
-      .spyOn(pokemonService, 'obtainPokemons')
-      .mockResolvedValue([pokemon])
+      render(<Home />)
+      expect(serviceMock).toHaveBeenCalledTimes(1)
 
-    render(<Home />)
-    expect(serviceMock).toHaveBeenCalledTimes(1)
-    const input = screen.getByRole('textbox')
-    fireEvent.change(input, { target: { value: 'bulbasaur' } })
+      await waitFor(() => {
+        expect(screen.getByText('Bulbasaur')).toBeInTheDocument()
+        expect(screen.getByText(`#001`)).toBeInTheDocument()
+        expect(screen.getByText('Grass')).toBeInTheDocument()
+        expect(screen.getByText('Poison')).toBeInTheDocument()
+        expect(screen.getByAltText('bulbasaur')).toHaveAttribute(
+          'src',
+          'bulbasaur-image-url',
+        )
+        expect(screen.getByText('6.9 kg')).toBeInTheDocument()
+        expect(screen.getByText('0.7 m')).toBeInTheDocument()
 
-    await waitFor(() => {
-      expect(screen.getByText('Bulbasaur')).toBeInTheDocument()
+        expect(screen.getByText('HP').nextElementSibling).toHaveTextContent(
+          '045',
+        )
+        expect(screen.getByText('ATK').nextElementSibling).toHaveTextContent(
+          '049',
+        )
+        expect(screen.getByText('DEF').nextElementSibling).toHaveTextContent(
+          '049',
+        )
+        expect(screen.getByText('SAT').nextElementSibling).toHaveTextContent(
+          '065',
+        )
+        expect(screen.getByText('SDF').nextElementSibling).toHaveTextContent(
+          '065',
+        )
+        expect(screen.getByText('SPD').nextElementSibling).toHaveTextContent(
+          '045',
+        )
+      })
     })
 
-    const pokemonCardElements = await screen.findAllByTestId('pokemon-card')
-    expect(pokemonCardElements.length).toBe(1)
-  })
+    test('Si un usuario busca un pokemon existente solo se muestra este', async () => {
+      const pokemon = new PokemonBuilder().build()
 
-  test('Si un usuario introduce letras que coinciden con mas de un pokemon se muestran estos (ej: char, se deberia de ver Charmander, Charmeleon, Charizard)', async () => {
-    const charmander = new PokemonBuilder()
-      .withName('charmander')
-      .withId(4)
-      .build()
-    const charmeleon = new PokemonBuilder()
-      .withName('charmeleon')
-      .withId(5)
-      .build()
-    const charizard = new PokemonBuilder()
-      .withName('charizard')
-      .withId(6)
-      .build()
+      const serviceMock = vi
+        .spyOn(pokemonService, 'obtainPokemons')
+        .mockResolvedValue([pokemon])
 
-    const serviceMock = vi
-      .spyOn(pokemonService, 'obtainPokemons')
-      .mockResolvedValue([charmander, charmeleon, charizard])
+      render(<Home />)
+      expect(serviceMock).toHaveBeenCalledTimes(1)
+      const input = screen.getByRole('textbox')
+      fireEvent.change(input, { target: { value: 'bulbasaur' } })
 
-    render(<Home />)
-    expect(serviceMock).toHaveBeenCalledTimes(1)
+      await waitFor(() => {
+        expect(screen.getByText('Bulbasaur')).toBeInTheDocument()
+      })
 
-    const input = screen.getByRole('textbox')
-    fireEvent.change(input, { target: { value: 'char' } })
-
-    await waitFor(() => {
-      expect(screen.getByText('Charmander')).toBeInTheDocument()
-      expect(screen.getByText('Charmeleon')).toBeInTheDocument()
-      expect(screen.getByText('Charizard')).toBeInTheDocument()
+      const pokemonCardElements = await screen.findAllByTestId('pokemon-card')
+      expect(pokemonCardElements.length).toBe(1)
     })
 
-    const pokemonCardElements = await screen.findAllByTestId('pokemon-card')
-    expect(pokemonCardElements.length).toBe(3)
+    test('Si un usuario introduce letras que coinciden con mas de un pokemon se muestran estos (ej: char, se deberia de ver Charmander, Charmeleon, Charizard)', async () => {
+      const charmander = new PokemonBuilder()
+        .withName('charmander')
+        .withId(4)
+        .build()
+      const charmeleon = new PokemonBuilder()
+        .withName('charmeleon')
+        .withId(5)
+        .build()
+      const charizard = new PokemonBuilder()
+        .withName('charizard')
+        .withId(6)
+        .build()
+
+      const serviceMock = vi
+        .spyOn(pokemonService, 'obtainPokemons')
+        .mockResolvedValue([charmander, charmeleon, charizard])
+
+      render(<Home />)
+      expect(serviceMock).toHaveBeenCalledTimes(1)
+
+      const input = screen.getByRole('textbox')
+      fireEvent.change(input, { target: { value: 'char' } })
+
+      await waitFor(() => {
+        expect(screen.getByText('Charmander')).toBeInTheDocument()
+        expect(screen.getByText('Charmeleon')).toBeInTheDocument()
+        expect(screen.getByText('Charizard')).toBeInTheDocument()
+      })
+
+      const pokemonCardElements = await screen.findAllByTestId('pokemon-card')
+      expect(pokemonCardElements.length).toBe(3)
+    })
   })
 })
