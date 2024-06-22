88e65b3e9528d818e3674c7eb5ea07a86ea8e635
Zhaklina Dobromirova <zhaklina@Air-de-Zhaklina.gestionwifi>
2024-06-22 10:24:17 +0200

    Añadido filtrar por tipo

--- src/components/Cards/CardsList.tsx
+++ src/components/Cards/CardsList.tsx
@@ -26,10 +26,11 @@ export const CardList: React.FC<CardSearchedType> = ({
     return <Skeleton />
   }
 
-  const searchedPokemons = pokemons.filter(pokemon =>
-    pokemon.name.toLowerCase().includes(hasSearched.toLowerCase()),
+  const searchedPokemons = pokemons.filter(
+    pokemon =>
+      pokemon.name.toLowerCase().includes(hasSearched.toLowerCase()) ||
+      pokemon.types.some(type => type.includes(hasSearched.toLowerCase())),
   )
-
   const hasSearchError = searchedPokemons.length === 0
 
   if (hasSearchError) {
