6df07505af591dec7fd4fc060074a2cf094bb18f
amlndz <alejandromelendez2014@gmail.com>
2024-06-24 13:44:34 +0200

    Estilo en desplegables filtros

--- src/components/FilterSelector/FilterSelector.tsx
+++ src/components/FilterSelector/FilterSelector.tsx
@@ -1,5 +1,7 @@
 import { PokemonType } from '../../domain/models/Pokemon'
 import { MultiSelect } from 'primereact/multiselect'
+import styles from '../Cards/PokemonCard/PokemonCard.module.css' // Importa los estilos de las cards de Pokémon
+import { PokemonColorIcon } from '../Cards/PokemonColorIcon'
 
 type FilterSelectorProps = {
   typeFilter: PokemonType[]
@@ -39,15 +41,21 @@ export const FilterSelector: React.FC<FilterSelectorProps> = ({
   const handleChange = (e: { value: Option[] }) => {
     let selectedTypes = e.value.map(option => option.name)
     if (selectedTypes.length > 2) {
-      selectedTypes.slice()
+      selectedTypes = selectedTypes.slice(0, 2) // Limita a máximo dos selecciones
     }
     setTypeFilter(selectedTypes)
   }
 
+  const selectedOptions = options.filter(option =>
+    typeFilter.includes(option.name),
+  )
+
   return (
-    <div>
+    <div className={styles['filter-selector']}>
+      {' '}
+      {/* Nuevo contenedor para estilos personalizados */}
       <MultiSelect
-        value={options.filter(option => typeFilter.includes(option.name))}
+        value={selectedOptions}
         onChange={handleChange}
         options={options}
         optionLabel="name"
@@ -56,6 +64,20 @@ export const FilterSelector: React.FC<FilterSelectorProps> = ({
         maxSelectedLabels={2}
         selectedItemsLabel="selected"
         showSelectAll={false}
+        className={styles['pokemon-pills']} // Aplica el estilo de los pills de los tipos de Pokémon
+        itemTemplate={(option: Option) => (
+          <div
+            className={styles['pokemon-type']}
+            style={{ backgroundColor: PokemonColorIcon[option.name].color }}
+          >
+            <img
+              src={PokemonColorIcon[option.name].icon}
+              className={styles['type-icon']}
+            />
+            <span>{option.name}</span>
+          </div>
+        )}
+        panelClassName={styles['filter-panel']} // Estilos para el panel desplegable
       />
     </div>
   )
